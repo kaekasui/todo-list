8b85f37b244b137935595d3b4ef09850ff3f55a1
@kae_kasui <kaekasui_github@icloud.com>
2014-05-22 15:09:12 +0900

    'rails generate migration RenameCreatedByToUserId user_id:integer' and 'rake db:migrate'

---
+++ db/migrate/20140522060320_rename_created_by_to_user_id.rb
@@ -0,0 +1,4 @@
+class RenameCreatedByToUserId < ActiveRecord::Migration
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140522055758) do
+ActiveRecord::Schema.define(version: 20140522060320) do
 
   create_table "tasks", force: true do |t|
     t.string   "name"

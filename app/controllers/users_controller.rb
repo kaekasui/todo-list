f59be990fb0cd1e07d8e8964bba2e1a9a74140c0
@kae_kasui <kaekasui_github@icloud.com>
2014-05-22 18:59:12 +0900

    add function to update user page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,23 @@
 class UsersController < ApplicationController
   before_action :login_required
+  before_action :set_user, only: [:edit, :update]
+  respond_to :html
   
   def edit
     @user = current_user
   end
 
   def update
+    @user.update(user_params)
+    respond_with @user, location: tasks_path
   end
+
+  private
+    def user_params
+      params.require(:user).permit(:mail)
+    end
+
+    def set_user
+      @user = User.find(params[:id])
+    end
 end 

39b946bf267a0b6cdf3e92fc602855b007ebec34
@kae_kasui <kaekasui_github@icloud.com>
2014-05-22 18:14:31 +0900

    'rails generate migration AddMailToUsers mail' and 'rake db:migrate'

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140522090222) do
+ActiveRecord::Schema.define(version: 20140522091409) do
 
   create_table "tasks", force: true do |t|
     t.string   "name"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20140522090222) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.datetime "deleted_at"
+    t.string   "mail"
   end
 
 end

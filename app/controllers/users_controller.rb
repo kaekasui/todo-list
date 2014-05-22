06cba49c558048bcb671b55b5d3bcd26b9fccb9e
@kae_kasui <kaekasui_github@icloud.com>
2014-05-22 18:25:40 +0900

    add user edit page

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,9 @@
+class UsersController < ApplicationController
+  before_action :login_required
+  
+  def edit
+  end
+
+  def update
+  end
+end 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Todo::Application.routes.draw do
   root 'home#index'
   resources :tasks, except: [:show]
+  resources :users, only: [:edit, :update]
   get '/auth/:provider/callback', to: 'sessions#callback'
   get '/logout' => 'sessions#destroy', as: :logout
 end
